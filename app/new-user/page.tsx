b80c6f300544eaadd77e0bab957965360f627693
Thiem <thiem.huynh0@gmail.com>
2023-08-22 19:39:53 -0400

    page redirect and new user creation check

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,31 @@
-const NewUser = () => {
-  return <div>Welcome New User!</div>;
+import { prisma } from '@/utils/db';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+const createNewUser = async () => {
+  const user = await currentUser();
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    },
+  });
+
+  // if somehow the user is not a user in the db. It is a new user. Edge Case
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+
+  redirect('/journal');
+};
+
+const NewUser = async () => {
+  await createNewUser();
+  return <div>...Loading</div>;
 };
 
 export default NewUser;
