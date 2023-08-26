e13be9217cf1af32dc49f15a31238ebc8459ff3d
Thiem <thiem.huynh0@gmail.com>
2023-08-26 13:40:37 -0400

    added search function to journal page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,30 @@
 import { UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
+
+const links = [
+  { href: '/', label: 'Home' },
+  { href: '/journal', label: 'Journal' },
+];
 
 const DashboardLayout = ({ children }) => {
   return (
-    <div className="h-screen relative">
+    <div className="h-screen relative ">
       <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
-        Mood
+        <div className="border-solid border-b border-black/10">
+          Mood Journal
+        </div>
+        <ul className="grid grid-cols-1 gap-1">
+          {links.map((link) => (
+            <li
+              key={link.href}
+              className="text-center px-2 py-6 text-xl border-solid border-b border-black/10 hover: hover:bg-indigo-500 duration-500"
+            >
+              <Link href={link.href} className="">
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </aside>
 
       <div className="ml-[200px] h-full">
