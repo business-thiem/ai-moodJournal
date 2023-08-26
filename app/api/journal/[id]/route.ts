108680eb7ca0a2ac0ada319308516753f20eba66
Thiem <thiem.huynh0@gmail.com>
2023-08-26 09:43:40 -0400

    prisma db upsert for analysis data

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,3 +1,4 @@
+import { analyze } from '@/utils/ai';
 import { getUserByClerkID } from '@/utils/auth';
 import { prisma } from '@/utils/db';
 import { NextResponse } from 'next/server';
@@ -15,6 +16,19 @@ export const PATCH = async (request, { params }) => {
     data: {
       content,
     },
+  });
+
+  // used upsert instead of update to create analysis on pre-existing data, but in production, we'd have db defaults and use update instead.
+  const analysis = await analyze(updatedEntry.content);
+  await prisma.analysis.upsert({
+    where: {
+      entryId: updatedEntry.id,
+    },
+    data: {
+      entryId: updatedEntry.id,
+      ...analysis,
+    },
+    update: analysis,
   });
 
   return NextResponse.json({ data: updatedEntry });
