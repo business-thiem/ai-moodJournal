43eed30af11bcb17ae5bd4685f198c678e039392
Thiem <thiem.huynh0@gmail.com>
2023-08-29 18:25:13 -0400

    better error logging on network requests

--- utils/api.ts
+++ utils/api.ts
@@ -3,42 +3,60 @@ const createURL = (path) => {
 };
 
 export const updateEntry = async (id, content) => {
-  const res = await fetch(
-    new Request(createURL(`/api/journal/${id}`), {
-      method: 'PATCH',
-      body: JSON.stringify({ content }),
-    })
-  );
+  try {
+    const res = await fetch(
+      new Request(createURL(`/api/journal/${id}`), {
+        method: 'PATCH',
+        body: JSON.stringify({ content }),
+      })
+    );
 
-  if (res.ok) {
-    const data = await res.json();
-    return data.data;
+    if (res.ok) {
+      const data = await res.json();
+      return data.data;
+    } else {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+  } catch (error) {
+    console.error(error);
   }
 };
 
 export const createNewEntry = async () => {
-  const res = await fetch(
-    new Request(createURL('/api/journal'), {
-      method: 'POST',
-    })
-  );
+  try {
+    const res = await fetch(
+      new Request(createURL('/api/journal'), {
+        method: 'POST',
+      })
+    );
 
-  if (res.ok) {
-    const data = await res.json();
-    return data.data;
+    if (res.ok) {
+      const data = await res.json();
+      return data.data;
+    } else {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+  } catch (error) {
+    console.error(error);
   }
 };
 
 export const askQuestion = async (question) => {
-  const res = await fetch(
-    new Request(createURL('/api/question'), {
-      method: 'POST',
-      body: JSON.stringify({ question }),
-    })
-  );
+  try {
+    const res = await fetch(
+      new Request(createURL('/api/question'), {
+        method: 'POST',
+        body: JSON.stringify({ question }),
+      })
+    );
 
-  if (res.ok) {
-    const data = await res.json();
-    return data.data;
+    if (res.ok) {
+      const data = await res.json();
+      return data.data;
+    } else {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+  } catch (error) {
+    console.error(error);
   }
 };
