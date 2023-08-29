3204375431ce188b386109581fcfaaaf526bfa9f
Thiem <thiem.huynh0@gmail.com>
2023-08-29 16:59:12 -0400

    providing ui to user when new entry is created.

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -2,15 +2,21 @@
 
 import { createNewEntry } from '@/utils/api';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const NewEntryCard = () => {
+  const [loading, setLoading] = useState(false);
+
   const router = useRouter();
   const handleOnClick = async () => {
+    setLoading(true);
     const data = await createNewEntry();
+    setLoading(false);
     router.push(`/journal/${data.id}`);
   };
   return (
     <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow">
+      {loading && <div>creating your new entry...loading</div>}
       <div className="px-4 py-5 sm:p-6" onClick={handleOnClick}>
         <span className="text-3xl">New Entry</span>
       </div>
