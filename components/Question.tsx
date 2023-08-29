ed6479c998b192d5aaf66f1457a29d518d97ffab
Thiem <thiem.huynh0@gmail.com>
2023-08-29 16:59:29 -0400

    question query for overall feel of journal entries

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,21 +1,30 @@
 'use client';
 
+import { askQuestion } from '@/utils/api';
 import { useState } from 'react';
 
 const Question = () => {
   const [value, setValue] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState();
   const onChange = (e) => {
     setValue(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault;
+    setLoading(true);
+    const answer = await askQuestion(value);
+    setResponse(answer);
+    setValue('');
+    setLoading(false);
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit} className="flex gap-1">
         <input
+          disabled={loading}
           onChange={onChange}
           value={value}
           type="text"
@@ -23,12 +32,15 @@ const Question = () => {
           className="w-1/2 border border-black/20 px-4 py-2 text-lg rounded-lg"
         />
         <button
+          disabled={loading}
           type="submit"
           className="bg-blue-400 px-4 py-2 text-lg rounded-lg"
         >
           Ask
         </button>
       </form>
+      {loading && <div>...loading</div>}
+      {response && <div>{response}</div>}
     </div>
   );
 };
